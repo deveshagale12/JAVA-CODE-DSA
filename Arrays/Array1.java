c743601303e7deb73baa6ed223bfca6c587301ac
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-03-05 17:49:52 +0530

    Array1

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -1,7 +1,8 @@
 
-public class Array1(){
-    public static void main(String args[]){
-        int arr[]={6732,32,444,57,23};
-        system.out.println(arr[1]);
-    }
+public class Array1 {
+	
+	 public static void main(String args[]){
+	        int arr[]={6732,32,444,57,23};
+	        System.out.println(arr[1]);
+	    }
 }
