c95469b387bda5b06f60689597743d97a04cf3b9
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-03-12 15:43:26 +0530

    MOVE ZEROS AT END

---
+++ Arrays/MOVEZEROSATEND.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MOVEZEROSATEND {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Size of an Array : ");
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		System.out.println("Enter Element in an Arrays : ");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+			
+		}
+		int count=0;
+		
+		for(int i=0;i<n;i++) {
+			if(arr[i]!=0) {
+				arr[count++]=arr[i];
+			}
+		
+		}
+		while(count < n) {
+		    arr[count++] = 0; // This fills all remaining spots
+		}
+		System.out.println("Display Array");
+		for(int i=0;i<n;i++) {
+			System.out.println(arr[i]);
+			
+		}
+	}
+
+}
