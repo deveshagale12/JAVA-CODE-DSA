e046e5852fb31df4e299030fe086f365973c2baf
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-04-23 17:45:16 +0530

    array

---
+++ Arrays/ArrayApna1.java
@@ -0,0 +1,22 @@
+package array;
+
+public class ArrayApna1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] marks=new int[9];
+        
+        marks[0]=23;
+        marks[1]=83;
+        marks[2]=36;
+        marks[3]=86;
+        marks[4]=85;
+        marks[5]=46;
+
+        for(int i=0;i<marks.length-1;i++) {
+        	System.out.println(marks[i]);
+        }
+
+	}
+
+}
