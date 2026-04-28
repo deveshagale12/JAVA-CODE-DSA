411fce27cbc438c841c0935bb1fb67c390460175
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-04-28 16:24:29 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial{
+
+    public static int fact(int n){
+        if(n<=1){
+            return 1;
+
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String args[]){
+        int num=9;
+
+        System.out.println(fact(num))
+    }
+}
