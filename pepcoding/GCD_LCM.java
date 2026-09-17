0605cf4b6fc19abf197c280a3ee0584c37691231
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-09-17 17:32:43 +0530

    code

---
+++ pepcoding/GCD_LCM.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class GCD_LCM {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int n1=sc.nextInt();
+		
+		System.out.println("Enter number : ");
+		int n2=sc.nextInt();
+		int on1=n1;
+		int on2=n2;
+		while(n1%n2!=0) {
+			int rem=n1%n2;
+			n1=n2;
+			n2=rem;
+			
+		}
+		int gcm=n2;
+		int lcm=(on1*on2)/gcm;
+		System.out.println("GCM : "+gcm);
+		System.out.println("LCM : "+lcm);
+
+	}
+}
